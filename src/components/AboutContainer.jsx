13ff3931ac489029309b8cccb5c51525dfca336e
André Mendes <develmendes@gmail.com>
2022-09-19 19:12:27 -0300

    about

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,15 @@
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint fuga impedit, cumque necessitatibus, iure harum ea culpa, ullam debitis consequuntur rerum. Eaque beatae enim nisi esse debitis eveniet, dicta id.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint fuga impedit, cumque necessitatibus, iure harum ea culpa, ullam debitis consequuntur rerum. Eaque beatae enim nisi esse debitis eveniet, dicta id.
+      </p>
+    </section>
+  )
+}
+
+export default AboutContainer
