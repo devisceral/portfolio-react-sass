94b452a959937bf7d7b33f43a26e3aa497b1859d
André Mendes <develmendes@gmail.com>
2022-09-19 19:15:06 -0300

    projects Container

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const ProjectsContainer = () => {
   return (
-    <div>ProjectsContainer</div>
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus dolor, deserunt possimus accusamus dicta nostrum quas non ipsa culpa aspernatur qui soluta accusantium quis similique officia tempora voluptate. Eligendi, similique.
+      </p>
+      <a href="" className="btn">Ver Projetos</a>
+    </section>
   )
 }
 
