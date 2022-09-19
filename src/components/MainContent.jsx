3efc321521e12790efb9719c96766d6662712014
André Mendes <develmendes@gmail.com>
2022-09-19 16:16:53 -0300

    flex maincontent

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import "../styles/components/maincontent.sass";
 
 const MainContent = () => {
   return (
-    <div>MainContent</div>
+    <main id="main-content">MainContent</main>
   )
 }
 
