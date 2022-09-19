5a63922f213f5e56c9d4942d9aaa978f5ffb2ee7
André Mendes <develmendes@gmail.com>
2022-09-19 15:21:35 -0300

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import Avatar from "../img/andremendes.jpg"
+
+import "../styles/components/sidebar.sass"
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside id="sidebar">
+      <img src={Avatar} alt="André Mendes" />
+      <p>Imagem</p>
+      <p classname="title">Desenvolvedor</p>
+      <p>redes sociais</p>
+      <p>informações de contato</p>
+      <a href="" className="btn">Download currículo</a>
+    </aside>
   )
 }
 
